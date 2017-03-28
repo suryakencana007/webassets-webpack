7a9afffe044068daf3ee2de13439917e15b7ba6b
Surya 'Zi <worksuryakencana@gmail.com>
2017-03-28 21:46:53 +0700

    try travis

--- webassets_webpack/__init__.py
+++ webassets_webpack/__init__.py
@@ -4,43 +4,42 @@ __all__ = ['Webpack']
 
 
 class Webpack(ExternalTool):
-	"""Use Webpack to bundle assets.
+    """Use Webpack to bundle assets.
 
-    Requires the Webpack executable to be available externally. You can
-    install it using `Node Package Manager <http://npmjs.org/>`_::
+        Requires the Webpack executable to be available externally. You can
+        install it using `Node Package Manager <http://npmjs.org/>`_::
 
         $ npm install webpack --save-dev
 
-    Supported configuration options:
+        Supported configuration options:
 
-    WEBPACK_BIN
+        WEBPACK_BIN
         The path to the Webpack binary. If not set, assumes ``Webpack``
         is in the system path.
-		
-		WEBPACK_CONFIG
-			Passed straight through to ``webpack --config`` to specify which webpack
-      config to use
 
-  """
+        WEBPACK_CONFIG
+        Passed straight through to ``webpack --config``
+        to specify which webpack config to use
+    """
 
-	name = 'webpack'
-	max_debug_level = None
-	options = {
-	'binary': 'WEBPACK_BIN',
-	'config': 'WEBPACK_CONFIG',
-  'run_in_debug': 'WEBPACK_RUN_IN_DEBUG',
-	}
+    name = 'webpack'
+    max_debug_level = None
+    options = {
+        'binary': 'WEBPACK_BIN',
+        'config': 'WEBPACK_CONFIG',
+        'run_in_debug': 'WEBPACK_RUN_IN_DEBUG',
+    }
 
-	def setup(self):
-		super(Webpack, self).setup()
-		if self.run_in_debug is False:
-			# Disable running in debug mode for this instance
-			self.max_debug_level = False
+    def setup(self):
+        super(Webpack, self).setup()
+        if self.run_in_debug is False:
+            # Disable running in debug mode for this instance
+            self.max_debug_level = False
 
-	def input(self, _in, out, **kw):
-		args = [self.binary or 'webpack']
+    def input(self, _in, out, **kw):
+        args = [self.binary or 'webpack']
 
-		if self.config:
-			args.extend(('--config', self.config))
+        if self.config:
+            args.extend(('--config', self.config))
 
-			return self.subprocess(args, out, _in)
+            return self.subprocess(args, out, _in)
